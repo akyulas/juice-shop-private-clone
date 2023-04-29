a0ecbd8a2af27fd23f338d4a2c6cdac5db1abb36
Jodi Oo <bolshack_fire@hotmail.com>
2023-04-29 14:39:27 +0800

    Remove injection-sanitizer

--- injection-sanitizer/identify.ts
+++
@@ -1,9 +0,0 @@
-const spawner = require('node:child_process').spawn;
-
-const data_sample = [ '{ $ne : -1 }' ]
-const python_process = spawner('python', ['./identify.py', JSON.stringify( data_sample )])
-python_process.stdout.on('data', (data) =>{
-    console.log( data.toString() );
-});
-
-// node identify.ts
